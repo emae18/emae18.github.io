e543836cf45fb97f34a27e43eb97c1daaeb53104
emae18 <eliasvilte1@gmail.com>
2020-08-01 00:43:49 -0300

    gatsby page

---
+++ component---src-pages-index-js-e2f2c3db84f3cdd41aee.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{RXBc:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),c=t.n(a),o=(t("Wbzz"),t("Bl7J")),r=t("vrFN");t("pxef");n.default=function(){return c.a.createElement(o.a,null,c.a.createElement(r.a,{title:"Home"}))}},pxef:function(e,n,t){}}]);
+//# sourceMappingURL=component---src-pages-index-js-e2f2c3db84f3cdd41aee.js.map
