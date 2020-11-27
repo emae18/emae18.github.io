1e7fbed75544893868b703738e45b9bd05ed3e26
emae18 <eliasvilte1@gmail.com>
2020-11-27 05:28:29 -0300

    background Image

---
+++ component---src-pages-about-js-414954155a7813032a22.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{"3XHS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("Bl7J"),r=n("vrFN"),c=n("S5hg");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(u.a,null,l.a.createElement(r.a,{title:"About"}),l.a.createElement("h1",null,"About us...")))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-414954155a7813032a22.js.map
---
+++ component---src-pages-page-2-js-4da7ba2faff424902488.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),c=a("Bl7J"),r=a("vrFN"),p=a("S5hg");t.default=function(){return l.a.createElement(p.a,null,l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage")))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-4da7ba2faff424902488.js.map
